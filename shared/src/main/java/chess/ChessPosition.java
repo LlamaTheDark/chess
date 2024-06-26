25c799b8877cad08df9e64699387475ba8ef3758
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-26 17:03:23 -0600

    Add runtime exception to ensure proper position bounds

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -11,6 +11,12 @@ public class ChessPosition {
     private final int col;
 
     public ChessPosition(int row, int col) {
+        if (row > 8 || col > 8){
+            throw new RuntimeException(String.format("""
+                    Suggested chess position cannot be greater than 8.
+                    Requested position creation: (row) %d, (col) %d
+                    """, row, col));
+        }
         this.row = row;
         this.col = col;
     }
