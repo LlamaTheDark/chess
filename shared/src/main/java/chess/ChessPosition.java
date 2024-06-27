dbbbe3397da7838ff9c9f1d148a77479329c5cde
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-27 14:01:33 -0600

    override equals() and hashCode() functions for ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,36 +1,51 @@
 package chess;
 
+import java.util.Objects;
+
 /**
- * Represents a single square position on a chess board
+ * Represents a single square position on a chess board.
+ * Board indices range is [1,8].
  * <p>
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
 public class ChessPosition {
-    private final int row;
-    private final int col;
+    private final int ROW;
+    private final int COL;
 
     public ChessPosition(int row, int col) {
-        if (row > 8 || col > 8){
+        if (row > chess.ChessBoard.BOARD_SIZE || col > chess.ChessBoard.BOARD_SIZE){
             throw new RuntimeException(String.format("""
                     Suggested chess position cannot be greater than 8.
                     Requested position creation: (row) %d, (col) %d
                     """, row, col));
         }
-        this.row = row;
-        this.col = col;
+        this.ROW = row;
+        this.COL = col;
     }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
-    public int getRow() { return this.row; }
+    public int getRow() { return this.ROW; }
 
     /**
      * @return which column this position is in
      * 1 codes for the left row
      */
-    public int getColumn() { return this.col; }
+    public int getColumn() { return this.COL; }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return getRow() == that.getRow() && getColumn() == that.getColumn();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRow(), getColumn());
+    }
 }
