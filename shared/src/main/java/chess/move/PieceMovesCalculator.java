21a32f6ba666e979404db131cb38ced4f71a6cb8
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-28 11:38:52 -0600

    fix: remove unused code

--- shared/src/main/java/chess/move/PieceMovesCalculator.java
+++ shared/src/main/java/chess/move/PieceMovesCalculator.java
@@ -8,16 +8,5 @@ import chess.ChessPosition;
 import java.util.Collection;
 
 public interface PieceMovesCalculator {
-    enum PositionType {
-        NULL,
-        CAPTURE,
-        FRIEND,
-    }
-    static PositionType getPositionType(ChessBoard board, ChessPosition start, ChessPosition end) {
-        ChessPiece endPiece = board.getPiece(end);
-        if(endPiece == null) return PositionType.NULL;
-        else if(endPiece.getTeamColor() != board.getPiece(start).getTeamColor()) return PositionType.CAPTURE;
-        else return PositionType.FRIEND;
-    }
     Collection<ChessMove> pieceMoves(ChessBoard chessBoard, ChessPosition position);
 }
