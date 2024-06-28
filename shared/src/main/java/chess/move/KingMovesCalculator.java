177e78786f93cdf6235e30d75cb0bc71eb565813
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-28 15:34:40 -0600

    pass test(King Move)

--- shared/src/main/java/chess/move/KingMovesCalculator.java
+++ shared/src/main/java/chess/move/KingMovesCalculator.java
@@ -4,10 +4,22 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class KingMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard chessBoard, ChessPosition position){
-        throw new UnsupportedOperationException("Not supported yet.");
+        var validMoves = new HashSet<ChessMove>();
+
+        PieceMovesCalculator.validateMovesFromVector(new int[]{0, 1}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{1, 0}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{0, -1}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{-1, 0}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{ 1,  1}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{-1,  1}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{-1, -1}, validMoves, chessBoard, position, false);
+        PieceMovesCalculator.validateMovesFromVector(new int[]{ 1, -1}, validMoves, chessBoard, position, false);
+
+        return validMoves;
     }
 }
