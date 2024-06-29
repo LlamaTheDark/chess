98926112782c14c3c8f762e8f967e9e77c3145ac
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-29 10:45:59 -0600

    docs: make comments more accurate

--- shared/src/main/java/chess/move/PawnMovesCalculator.java
+++ shared/src/main/java/chess/move/PawnMovesCalculator.java
@@ -21,24 +21,21 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         int[] pawnData = (chessBoard.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE)
                 ? new int[]{ 1, 2, 7}
                 : new int[]{-1, 7, 2};
-        /*
-        If the pawn is in the starting location, he can move two ahead of him.
-        Otherwise, he can move one ahead of him.
-
-        If there is an enemy in front of him, he cannot take it. He only captures diagonally forward.
-
-        also remember to take into account whether it's a white or black pawn
-         */
 
         int maxForwardLength; boolean promotionPiece = false;
         if(position.getRow() == pawnData[1]){
+            // if the pawn's row is his starting row, he can move forward up to 2 squares
             maxForwardLength = 2;
         }else{
             maxForwardLength = 1;
+
+            // if this pawn is in his opponent's starting row, his next valid move will promote him.
             if(position.getRow() == pawnData[2]) promotionPiece = true;
         }
+        // move forward
         PieceMovesCalculator.validateMovesFromVector(new int[]{pawnData[0], 0}, validMoves, chessBoard, position, maxForwardLength, false, true, promotionPiece);
 
+        // capture diagonally
         PieceMovesCalculator.validateMovesFromVector(new int[]{pawnData[0], 1}, validMoves, chessBoard, position, 1, true, false, promotionPiece);
         PieceMovesCalculator.validateMovesFromVector(new int[]{pawnData[0], -1}, validMoves, chessBoard, position, 1, true, false, promotionPiece);
 
