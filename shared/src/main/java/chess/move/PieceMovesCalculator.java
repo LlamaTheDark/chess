150a472ec00a7322a4af6b4f287be14aa8e25919
Seamus Morrison <seamus.pegden@outlook.com>
2024-06-29 10:15:33 -0600

    docs: add more detailed documentation for validateMovesFromVector()

--- shared/src/main/java/chess/move/PieceMovesCalculator.java
+++ shared/src/main/java/chess/move/PieceMovesCalculator.java
@@ -10,7 +10,8 @@ import java.util.HashSet;
 
 public interface PieceMovesCalculator {
     /**
-     * Loops through each position along the line of the given direction vector from the initial position.
+     * Loops through each position along the line of the given direction vector from the initial position and
+     * adds valid moves to the move set.
      * @param directionVector An integer array of size 2: {row offset, column offset}
      * @param validMoves A HashSet which will store the moves which have been validated.
      * @param board The board on which the pieces are arranged.
@@ -20,6 +21,8 @@ public interface PieceMovesCalculator {
      *                   into enemy space. True by default.
      * @param allowNull A boolean value. True: allows movement into an empty space. False: does not allow movement
      *                  into an empty space. True by default.
+     * @param promotionPiece A boolean value. True: if this is a valid move, this move will promote the piece in
+     *                       question. False: This move cannot promote a piece.
      */
     static void validateMovesFromVector(int[] directionVector, HashSet<ChessMove> validMoves, ChessBoard board,
                                         ChessPosition startPosition, int maxLineLength, boolean canCapture,
@@ -40,9 +43,9 @@ public interface PieceMovesCalculator {
                     || endPosition.getRow() < 1 || endPosition.getColumn() < 1) break;
 
             if(board.getPiece(endPosition) != null){
-                // is a valid move if it's not null, but it is an enemy
-                validMove = board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()
-                        && canCapture;
+                // is a valid move if it's not null, but it is an enemy (and this validation allows capturing)
+                validMove = canCapture &&
+                        board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor();
 
                 // if we hit something that's not null, we'll have to stop whether we add the move or not
                 stop = true;
@@ -57,6 +60,7 @@ public interface PieceMovesCalculator {
                     validMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.BISHOP));
                     validMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.ROOK));
                     validMoves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KNIGHT));
+
                 // otherwise, just add the one
                 }else{
                     validMoves.add(new ChessMove(startPosition, endPosition));
