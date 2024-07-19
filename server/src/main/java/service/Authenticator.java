bfd592086d5fc982a94284095f2076a319efa5e8
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-19 17:23:46 -0600

    refactor: handle authentication in dedicated function

--- server/src/main/java/service/Authenticator.java
+++ server/src/main/java/service/Authenticator.java
@@ -1,13 +1,18 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.memory.MemoryAuthDAO;
+import service.error.UnauthorizedException;
+
 import java.util.UUID;
 
 public class Authenticator {
-    public static boolean authenticate(String authToken) {
-        /*
-        TODO: implement
-         */
-        return true;
+    public static void authenticate(String authToken) throws DataAccessException, UnauthorizedException {
+        AuthDAO authDAO = new MemoryAuthDAO();
+        if(authDAO.getAuthByToken(authToken) == null){
+            throw new UnauthorizedException("Error: unauthorized");
+        }
     }
 
     public static String generateToken() {
--- server/src/main/java/service/user/LogoutService.java
+++ server/src/main/java/service/user/LogoutService.java
@@ -1,23 +1,19 @@
 package service.user;
 
-import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.memory.MemoryAuthDAO;
 import exchange.user.LogoutRequest;
 import exchange.user.LogoutResponse;
+import service.Authenticator;
 import service.Service;
 import service.error.ServiceException;
-import service.error.UnauthorizedException;
 
 public class LogoutService implements Service<LogoutResponse, LogoutRequest> {
     @Override
     public LogoutResponse serve(LogoutRequest request) throws DataAccessException, ServiceException {
-        AuthDAO authDAO = new MemoryAuthDAO();
-        if(authDAO.getAuthByToken(request.getAuthToken()) == null){
-            throw new UnauthorizedException("Error: unauthorized");
-        }
+        Authenticator.authenticate(request.getAuthToken());
 
-        authDAO.deleteAuth(request.getAuthToken());
+        new MemoryAuthDAO().deleteAuth(request.getAuthToken());
         return new LogoutResponse();
     }
 }
