a997746dc1680e007a353a96693d264fc5e765b7
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-19 17:46:27 -0600

    refactor: create default messages for relevant errors

--- server/src/main/java/service/error/UnauthorizedException.java
+++ server/src/main/java/service/error/UnauthorizedException.java
@@ -2,4 +2,7 @@ package service.error;
 
 public class UnauthorizedException extends ServiceException {
     public UnauthorizedException(String message) { super(message); }
+    public UnauthorizedException(){
+        super("Error: unauthorized");
+    }
 }
