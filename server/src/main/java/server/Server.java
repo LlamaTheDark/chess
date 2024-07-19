6345565ce9eb80ac931070700481ed4576f63597
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-19 17:57:02 -0600

    refactor: distribute endpoint registration into different functions
    new: add endpoint to handle joining a game

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 import handle.dev.ClearApplicationHandler;
 import handle.game.CreateGameHandler;
+import handle.game.JoinGameHandler;
 import handle.user.LoginHandler;
 import handle.user.LogoutHandler;
 import handle.user.RegisterHandler;
@@ -15,17 +16,28 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.post("/user", new RegisterHandler());
-        Spark.post("/session", new LoginHandler());
-        Spark.delete("/session", new LogoutHandler());
-        Spark.post("/game", new CreateGameHandler());
-        Spark.delete("/db", new ClearApplicationHandler());
+        registerUserEndpoints();
+        registerGameEndpoints();
+        registerDevEndpoints();
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private void registerUserEndpoints() {
+        Spark.post("/user", new RegisterHandler());
+        Spark.post("/session", new LoginHandler());
+        Spark.delete("/session", new LogoutHandler());
+    }
+    private void registerGameEndpoints() {
+        Spark.post("/game", new CreateGameHandler());
+        Spark.put("/game", new JoinGameHandler());
+    }
+    private void registerDevEndpoints() {
+        Spark.delete("/db", new ClearApplicationHandler());
     }
 
     public void stop() {
