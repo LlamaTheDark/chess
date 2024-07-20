a5b70664ca9b3bf52be284fb3f29fb172560b61a
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-20 17:32:33 -0600

    docs(Serializer): provide javadocs

--- server/src/main/java/handle/util/Serializer.java
+++ server/src/main/java/handle/util/Serializer.java
@@ -2,25 +2,43 @@ package handle.util;
 
 import com.google.gson.Gson;
 
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Serializes and deserializes JSON objects using <a href="https://github.com/google/gson">Google's Gson Library</a>.
+ */
 public
 class Serializer {
     static Gson gson = new Gson();
 
+    /**
+     * Serializes an object.
+     *
+     * @param obj The object to be serialized.
+     *
+     * @return A <code>String</code> containing the serialized object.
+     */
     static public
     String serialize(Object obj) {
         return gson.toJson(obj);
     }
 
+    /**
+     * Deserializes an object.
+     *
+     * @param obj   The String containing the serialized object.
+     * @param clazz The class of the object to
+     * @param <T>   The type variable which is of the same type as the class.
+     *
+     * @return The deserialized object, or a new instance of the object if <code>obj</code> is <code>null</code>.
+     */
     static public
-    <T> T deserialize(String obj, Class<T> clazz) {
+    <T> T deserialize(String obj, Class<T> clazz)
+    throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         var deserialized = gson.fromJson(obj, clazz);
 
-        try {
-            if (deserialized == null) {
-                deserialized = clazz.getDeclaredConstructor().newInstance();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (deserialized == null) {
+            deserialized = clazz.getDeclaredConstructor().newInstance();
         }
 
         return deserialized;
