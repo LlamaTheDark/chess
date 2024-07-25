1e9dbe40d52fd4d42aa2e59100aa8f513472fa5d
Seamus Morrison <seamus.pegden@outlook.com>
2024-07-25 16:13:33 -0600

    refactor: create constructor without gameID

--- server/src/main/java/model/GameData.java
+++ server/src/main/java/model/GameData.java
@@ -4,4 +4,16 @@ import chess.ChessGame;
 
 public
 record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    public
+    GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this(0, whiteUsername, blackUsername, gameName, game);
+    }
 }
