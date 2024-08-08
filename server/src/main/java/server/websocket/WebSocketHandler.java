531101f98ce93d4ea466d0fb6437441975a7a9dc
Seamus Morrison <seamus.pegden@outlook.com>
2024-08-08 17:51:36 -0600

    fix: re-init commands and messages for websocket communication

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,14 +1,14 @@
 package server.websocket;
 
-import exchange.fullduplex.websocket.commands.ConnectCommand;
-import exchange.fullduplex.websocket.commands.LeaveGameCommand;
-import exchange.fullduplex.websocket.commands.MakeMoveCommand;
-import exchange.fullduplex.websocket.commands.ResignGameCommand;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.ConnectCommand;
+import websocket.commands.LeaveGameCommand;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.ResignGameCommand;
 
 @WebSocket
 public
---
+++ shared/src/main/java/websocket/commands/ResignGameCommand.java
@@ -0,0 +1,9 @@
+package websocket.commands;
+
+public
+class ResignGameCommand extends UserGameCommand {
+    public
+    ResignGameCommand(CommandType commandType, String authToken, Integer gameID) {
+        super(commandType, authToken, gameID);
+    }
+}
